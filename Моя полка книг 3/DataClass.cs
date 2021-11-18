8177dc5c62829b52bcede64049122e73c0eee584
Admin <Admin@DESKTOP-02F739R>
2021-11-18 09:52:50 +0300

    Добавьте файлы проекта.

---
+++ Моя полка книг 3/DataClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Моя_полка_книг_3
+{
+    class DataClass
+    {
+      
+    }
+}
---
+++ Моя полка книг 3/global.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Моя_полка_книг_3
+{
+    class global
+
+    {
+
+        string StringConnection = "server=localhost; user id=root; password=12345;database=database_dk";
+
+
+        public string CheckConnection()
+        {
+            string answer = string.Empty;
+            using (MySqlConnection connection = new MySqlConnection(StringConnection))
+            {
+
+                connection.Open();
+
+                string SQL = "SELECT * FROM database_bk.bооks;";
+
+                MySqlCommand cmd = new MySqlCommand(SQL, connection);
+                answer = cmd.ExecuteScalar().ToString();
+            }
+
+            return answer;
+        }
+    }
